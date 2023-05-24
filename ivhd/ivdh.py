fcbf96fd432bc3606a615674cab5f581ec66fbd0
pawlo555 <spawel555@outlook.com>
2023-05-24 20:09:41 +0200

    IVHD implementation using torch.autograd and optimizers

--- ivhd/ivdh.py
+++ ivhd/ivdh.py
@@ -1,4 +1,7 @@
+from typing import Optional, Type
+
 import torch
+from torch.optim import Optimizer
 
 
 class IVHD:
@@ -8,32 +11,58 @@ class IVHD:
             nn: int = 2,
             rn: int = 1,
             c: float = 0.1,
+            optimizer: Optional[Type[Optimizer]] = None,
             epochs: int = 200,
             eta: float = 1.) -> None:
         self.n_components = n_components
         self.nn = nn
         self.rn = rn
         self.c = c
+        self.optimizer = optimizer
         self.epochs = epochs
         self.eta = eta
         self.a = 0.2
         self.b = 1.
 
-    def fit_transform(self, X: torch.Tensor, NN: torch.Tensor, RN: torch.Tensor):
+    def fit_transform(self, X: torch.Tensor, NN: torch.Tensor, RN: torch.Tensor) -> torch.Tensor:
+        if self.optimizer is None:
+            return self.force_method(X, NN, RN)
+        else:
+            x = torch.rand((X.shape[0], 1, self.n_components), requires_grad=True)
+            NN = NN.reshape(-1)
+            RN = RN.reshape(-1)
+            optimizer = self.optimizer(params={x}, lr=0.5)
+            for i in range(self.epochs):
+                optimizer.zero_grad()
+                nn_diffs = x - torch.index_select(x, 0, NN).reshape(X.shape[0], -1, self.n_components)
+                rn_diffs = x - torch.index_select(x, 0, RN).reshape(X.shape[0], -1, self.n_components)
+                nn_dist = torch.sqrt(torch.sum((nn_diffs+1e-5) ** 2, dim=-1, keepdim=True))
+                rn_dist = torch.sqrt(torch.sum((rn_diffs+1e-5) ** 2, dim=-1, keepdim=True))
+
+                loss = torch.sum(nn_dist*nn_dist) + self.c*torch.sum((1-rn_dist)*(1-rn_dist))
+                print(f"\r{i} loss: {loss.item()}, X: {x[0]}", end="\n")
+                loss.backward()
+                optimizer.step()
+            return x[:, 0].detach()
+
+    def force_method(self, X: torch.Tensor, NN: torch.Tensor, RN: torch.Tensor) -> torch.Tensor:
         x = torch.randn((X.shape[0], 1, self.n_components))
         delta_x = torch.zeros_like(x)
         NN = NN.reshape(-1)
         RN = RN.reshape(-1)
         for i in range(self.epochs):
-            print(f"\r{i}", end="")
             nn_diffs = x - torch.index_select(x, 0, NN).reshape(X.shape[0], -1, self.n_components)
             rn_diffs = x - torch.index_select(x, 0, RN).reshape(X.shape[0], -1, self.n_components)
+            nn_dist = torch.sqrt(torch.sum(nn_diffs ** 2, dim=-1, keepdim=True))
             rn_dist = torch.sqrt(torch.sum(rn_diffs**2, dim=-1, keepdim=True))
 
             f_nn = torch.mean(nn_diffs, dim=1, keepdim=True)
             f_rn = torch.mean((1-rn_dist)/(rn_dist + 1e-5)*rn_diffs, dim=1, keepdim=True)
 
-            f = -f_nn + self.c*f_rn
+            loss = torch.sum(nn_dist**2) + torch.sum((1-rn_dist)**2)
+            print(f"\r{i} loss: {loss.item()}", end="")
+
+            f = -f_nn - self.c*f_rn
             delta_x = self.a*delta_x + self.b*f
             x = x + self.eta * delta_x
         return x[:, 0]
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import torch
 import torchvision
-import numpy as np
 import matplotlib.pyplot as plt
 
 from ivhd.ivdh import IVHD
+from torch import autograd
 
-N = 1500
-NN = 5
-RN = 2
+N = 2000
+NN = 2
+RN = 1
 
 if __name__ == '__main__':
     dataset = torchvision.datasets.MNIST("mnist", train=True, download=True)
@@ -15,17 +15,24 @@ if __name__ == '__main__':
     X = X.reshape(N, -1) / 255.
     print(X.shape, torch.max(X))
     Y = dataset.targets[:N]
-    ivhd = IVHD(2, 10, 2)
+    ivhd = IVHD(2, 10, 2, optimizer=torch.optim.Adam, epochs=200)
 
     rn = torch.randint(0, N, (N, RN))
 
     X_1 = X.reshape(N, 1, -1)
     X_2 = X.reshape(1, N, -1)
-    distances = torch.sum((X_1 - X_2)**2, dim=-1)
+    distances = torch.zeros(N, N)
+    for i in range(N):
+        print(f"\r{i}", end="")
+        distances[i] = torch.sum((X[i] - X)**2, dim=-1)
+
+    # old way, takes too much memory
+    #distances = torch.sum((X_1 - X_2)**2, dim=-1)
     _, nn = torch.topk(distances, NN+1, dim=-1, largest=False)
     nn = nn[:, 1:]
     print(distances.shape)
-    x = ivhd.fit_transform(X, nn, rn)
+    with autograd.detect_anomaly():
+        x = ivhd.fit_transform(X, nn, rn)
 
     fig = plt.figure()
     print(Y.shape)

180d4471334b61abdf9fd660e05d5eb26dee8b1d
pawlo555 <spawel555@outlook.com>
2023-05-30 21:56:20 +0200

    Loading and saving distances matrix

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from ivhd.ivdh import IVHD
 from torch import autograd
 
 N = 2000
-NN = 2
-RN = 1
+NN = 5
+RN = 2
 
 if __name__ == '__main__':
     dataset = torchvision.datasets.MNIST("mnist", train=True, download=True)
@@ -15,31 +15,44 @@ if __name__ == '__main__':
     X = X.reshape(N, -1) / 255.
     print(X.shape, torch.max(X))
     Y = dataset.targets[:N]
-    ivhd = IVHD(2, 10, 2, optimizer=torch.optim.Adam, epochs=200)
+    ivhd = IVHD(2, 10, 2, optimizer=torch.optim.Adagrad, epochs=10000)
 
     rn = torch.randint(0, N, (N, RN))
-
-    X_1 = X.reshape(N, 1, -1)
-    X_2 = X.reshape(1, N, -1)
     distances = torch.zeros(N, N)
-    for i in range(N):
-        print(f"\r{i}", end="")
-        distances[i] = torch.sum((X[i] - X)**2, dim=-1)
+    try:
+        result = torch.load("mnist_dist.pt")
+        print(result)
+        assert result["mnist"].shape == (N, N)
+        distances = result["mnist"]
+    except Exception or AssertionError:
+        for i in range(N):
+            print(f"\r{i}", end="")
+            distances[i] = torch.sum((X[i] - X)**2, dim=-1)
+
+    torch.save({"mnist": distances}, 'mnist_dist.pt')
+    #torch.load('mnist_dist.pt')
 
     # old way, takes too much memory
-    #distances = torch.sum((X_1 - X_2)**2, dim=-1)
-    _, nn = torch.topk(distances, NN+1, dim=-1, largest=False)
-    nn = nn[:, 1:]
-    print(distances.shape)
+    nn = torch.zeros(N, NN, dtype=torch.int32)
+    STEP = 1000
+    for i in range(0, N, STEP):
+        #print(i, i+STEP)
+        nn[i:i+STEP, :] = torch.topk(distances[i:i+STEP, :], NN+1, dim=-1, largest=False)[1][:, 1:]
+    #print(nn[:15])
+    _, nn_copy = torch.topk(distances, NN + 1, dim=-1, largest=False)
+    nn_copy = nn_copy[:, 1:]
+    #print(nn_copy[:15])
+    #print(torch.all(nn_copy == nn))
+    #print(distances.shape)
     with autograd.detect_anomaly():
         x = ivhd.fit_transform(X, nn, rn)
 
     fig = plt.figure()
-    print(Y.shape)
+    #print(Y.shape)
     plt.title("Mnist 2d visualization")
     for i in range(10):
         points = x[Y == i]
-        plt.scatter(points[:, 0], points[:, 1], label=f"{i}")
-    print(x)
+        plt.scatter(points[:, 0], points[:, 1], label=f"{i}", marker="x", alpha=0.5)
+    #print(x)
     plt.legend()
     plt.show()

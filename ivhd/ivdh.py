56d5c78c95f0e19f7d327b20dac8e9908d32b153
pawlo555 <spawel555@outlook.com>
2023-06-20 12:07:31 +0200

    Add sanity tests and mnist tests

--- ivhd/ivdh.py
+++ ivhd/ivdh.py
@@ -12,36 +12,46 @@ class IVHD:
             rn: int = 1,
             c: float = 0.1,
             optimizer: Optional[Type[Optimizer]] = None,
+            lr: float = 0.1,
             epochs: int = 200,
-            eta: float = 1.) -> None:
+            eta: float = 1.,
+            device: str = "cpu") -> None:
         self.n_components = n_components
         self.nn = nn
         self.rn = rn
         self.c = c
         self.optimizer = optimizer
+        self.lr = lr
         self.epochs = epochs
         self.eta = eta
         self.a = 0.2
         self.b = 1.
+        self.device = device
 
     def fit_transform(self, X: torch.Tensor, NN: torch.Tensor, RN: torch.Tensor) -> torch.Tensor:
+        X = X.to(self.device)
+        NN = NN.to(self.device)
+        RN = RN.to(self.device)
+
         if self.optimizer is None:
             return self.force_method(X, NN, RN)
         else:
-            x = torch.rand((X.shape[0], 1, self.n_components), requires_grad=True)
+            x = torch.rand((X.shape[0], 1, self.n_components), requires_grad=True, device=self.device)
             x_start = x.detach().clone()
             NN = NN.reshape(-1)
             RN = RN.reshape(-1)
-            optimizer = self.optimizer(params={x}, lr=10.)
+            optimizer = self.optimizer(params={x}, lr=self.lr)
             for i in range(self.epochs):
                 x_copy = x.detach().clone()
                 optimizer.zero_grad()
                 nn_diffs = x - torch.index_select(x_copy, 0, NN).reshape(X.shape[0], -1, self.n_components)
                 rn_diffs = x - torch.index_select(x_copy, 0, RN).reshape(X.shape[0], -1, self.n_components)
-                nn_dist = torch.sqrt(torch.sum((nn_diffs+1e-5) ** 2, dim=-1, keepdim=True))
-                rn_dist = torch.sqrt(torch.sum((rn_diffs+1e-5) ** 2, dim=-1, keepdim=True))
+                nn_dist = torch.sqrt(torch.sum((nn_diffs+1e-8) ** 2, dim=-1, keepdim=True))
+                rn_dist = torch.sqrt(torch.sum((rn_diffs+1e-8) ** 2, dim=-1, keepdim=True))
 
                 loss = torch.mean(nn_dist*nn_dist) + self.c*torch.mean((1-rn_dist)*(1-rn_dist))
+                if loss < 1e-10:
+                    return x[:, 0].detach()
                 print(f"\r{i} loss: {loss.item()}, X: {x[0]}", torch.mean(torch.abs(x-x_start)),
                       torch.max(torch.abs(x-x_start)), end="")
                 if i % 100 == 0:
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     X = X.reshape(N, -1) / 255.
     print(X.shape, torch.max(X))
     Y = dataset.targets[:N]
-    ivhd = IVHD(2, 10, 2, optimizer=torch.optim.Adagrad, epochs=10000)
+    ivhd = IVHD(2, NN, RN, optimizer=torch.optim.Adam, epochs=300)
 
     rn = torch.randint(0, N, (N, RN))
 
@@ -30,27 +30,11 @@ if __name__ == '__main__':
     graph.load_from_binary_file("./graph_files/out.bin", nn_count=NN)
     nn = torch.tensor(graph.indexes.astype(np.int32))
     print(nn)
-    # second old way
-    # X_1 = X.reshape(N, 1, -1)
-    # X_2 = X.reshape(1, N, -1)
-    # distances = torch.zeros(N, N)
-    # for i in range(N):
-    #     print(f"\r{i}", end="")
-    #     distances[i] = torch.sum((X[i] - X)**2, dim=-1)
-    # _, nn = torch.topk(distances, NN+1, dim=-1, largest=False)
-    # nn = nn[:, 1:]
-
-
-    # old way, takes too much memory
-    #distances = torch.sum((X_1 - X_2)**2, dim=-1)
-    # _, nn = torch.topk(distances, NN+1, dim=-1, largest=False)
-    # nn = nn[:, 1:]
 
     with autograd.detect_anomaly():
         x = ivhd.fit_transform(X, nn, rn)
 
     fig = plt.figure()
-    #print(Y.shape)
     plt.title("Mnist 2d visualization")
     for i in range(10):
         points = x[Y == i]
